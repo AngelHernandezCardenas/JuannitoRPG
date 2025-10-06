099333b395e92501996c3434d950bba98634584d
Alejandro Alvarez Lopez <140360399+AlejandroAlvarezLopez183@users.noreply.github.com>
2025-10-06 09:56:41 -0600

    actualizacion paquetes
    implementacion de paquetes

---
+++ src/conexionSQl/generador_Tabla.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQl;
+
+/**
+ *
+ * @author alejandro
+ */
+public class generador_Tabla {
+    
+}
